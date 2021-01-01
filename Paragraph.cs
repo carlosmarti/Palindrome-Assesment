45e8596a27c7ce9ea1397556f4c52d6fc80985fd
carlosmarti <carlos.marti29@yahoo.com>
2021-01-01 14:08:56 -0500

    added functionality to find words that are palindrome

--- Paragraph.cs
+++ Paragraph.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 
 /*
 A paragraph is a grouping of one or more sentences.
@@ -20,34 +21,125 @@ class Paragraph
     */
     class Sentence
     {
-        private int numOfPalin;
+        private int numOfPalinWords;
         private string sentence;
 
-        public int NumOfPalin { get{return numOfPalin;} }
+        public int NumOfPalinWords { get{return numOfPalinWords;} set{ numOfPalinWords = value;} }
         public string SentenceGetSet {get{return sentence;} set{sentence = value;}} 
 
-        Sentence(string sentence)
+        public Sentence(string sentence)
         {
             SentenceGetSet = sentence;
+
+            FindPalindromes();
         }
 
+        /*
+            <summary>
+                This method will check each word in the sentence for palindrome words.
+            </summary>
+        */
         public void FindPalindromes()
         {
+            string[] words = SentenceGetSet.Split();
+            for(int i = 0; i < words.Length; i++)
+            {
+                //quick fix for a pesky problem where a space is considered a word
+                if(String.IsNullOrWhiteSpace(words[i]))
+                    continue;
+                
 
+                //remove the problem if the first word in the sentence is capitalized
+                string word = words[i].ToLower();
+                
+                
+                if(word.Equals(reverse(word)))
+                {
+                    NumOfPalinWords += 1;
+                }
+            }
         }
 
+        /*
+            <summary>
+                This method takes in a word to which it will break into characters. Each character will be stored into a stack since a stack is a LIFO data structure.
+                Once all of the elements of the stack is poped the word will be reversed.
+            </summary>
+            <return>
+                a new string with the reversed word.
+            </return>
+        */
         private string reverse(string word)
         {
-            return null;
+            char[] wordCharArr = word.ToCharArray();
+            Stack<char> arrOfChar = new Stack<char>();
+
+            //push each letter into the stack
+            foreach(char letter in wordCharArr)
+            {
+                arrOfChar.Push(letter);
+            }
+
+            //pop stack into char array  
+            for(int i = 0; i < wordCharArr.Length; i++)
+            {
+                wordCharArr[i] = arrOfChar.Pop();
+            }
+
+            //create a new string with the reversed word
+            return new string(wordCharArr);
         }
     }
 
     private List<Sentence> sentences;
     private string paragraph;
 
-    Paragraph(string paragraph)
+    public Paragraph(string paragraph)
     {
         this.paragraph = paragraph;
+        sentences = new List<Sentence>();
+
+        PullSentences();
+    }
+
+    /*
+        <summary>
+            This method pulls out all of the sentences by using the Split method in the string object. After which each sentence string is passed to the Sentence object. 
+        </summary>
+    */
+    public void PullSentences()
+    {
+        string[] paraSentences = paragraph.Split(".");
+
+        foreach(string se in paraSentences)
+        {
+            //remove extra empty sentence caused by Split
+            if(String.IsNullOrWhiteSpace(se))
+                continue;
+            
+            sentences.Add(new Sentence(se));
+        }
+    }
+
+    /*
+        <summary>
+            This method goes through each sentence class and stores amount of palindrome words in each sentence.
+        </summary>
+
+        <return>
+            the accumulated amount of words that are palindrome.
+        </return>
+    */
+    public int GetNumOfPalinWords()
+    {
+        var amount = 0;
+
+        foreach(var sent in sentences)
+        {
+            amount += sent.NumOfPalinWords;
+        }
+
+        return amount;
     }
 
     public string GetParagraph()
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Pelindrome
 {
@@ -6,8 +7,19 @@ namespace Pelindrome
     {
         static void Main(string[] args)
         {
-            //to-do pass the paragraph to the Paragraph class
-            Console.WriteLine(args[0] + args[1]);
+            //Provide directory of where the txt file is located
+            var path = @"C:\Users\carlos\Documents\C-Sharp\Palindrome-Assesment\paragraph.txt";
+
+            //check if file is in directory 
+            if(!File.Exists(path))
+            {
+                throw new FileNotFoundException("File was not found in the directory given. Please provide correct directory.");
+            }
+
+            //Now that file has been confirmed to exist whe can pass in the contents into the paragraph class
+            Paragraph paragraph = new Paragraph(File.ReadAllText(path));
+
+            Console.WriteLine("Total amount of palindrome words: {0}", paragraph.GetNumOfPalinWords());
         }
     }
 }
